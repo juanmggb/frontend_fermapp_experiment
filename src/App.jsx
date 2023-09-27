61b4f162f294dd65c0f8b583b19a129a50ebe314
JuanM <58197473+juanmggb@users.noreply.github.com>
2023-09-26 21:58:16 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import RegisterExperiment from "./pages/RegisterExperiment";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <RegisterExperiment />
+    </>
+  );
+}
+
+export default App;
